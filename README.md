a546114d1173b8dc75f9abc8e4295ae2be72b7eb
johnny_wang <johnny1001@top-link.com.tw>
2019-05-29 12:02:58 +0800

    j init repo

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # jsPDF-html2canvas
 A combine usage with jsPDF &amp; html2canvas, which translating html content to PDF file.
+
+Also contains chinese unicode font js files, which is made by jsPDF's ttf-file-converter.
+
+## html2PDF(DOM, options)
+
+convert specific DOM target to print it into PDF file.
+
+Automatically, it'll save the file, or you can define the success function to do with the
+
+jsPDF instance.
+
+```html
+<!-- default a4's width is 595.28px -->
+<div id="page" style="width: 595.28px;color: black;background: white;">
+  <h3>PDF for Test</h3>
+  <p>Here is some content for testing!!</p>
+</div>
+
+<button id="btn">Generate</button>
+```
+
+```js
+let btn = document.getElementById('btn');
+let page = document.getElementById('page');
+
+btn.addEventListener('click', function(){
+  html2PDF(page, {
+    customFont: 'chinese'
+  });
+});
+```
+
+## options
+
+- **jsPDF**
+
+setting for creating jsPDF's instance
+
+```js
+let doc = new jsPDF(opts.jsPDF);
+```
+
+- **imageType**
+
+define the target imageType, now only support for jpeg, png, webp
+
+```js
+let pageData = canvas.toDataURL(opts.imageType, 1.0);
+```
+
+- **customFont**
+
+define customFont if needed, eg: chinese's unicode, must be used with importing the font's js module.
+
+refer: https://rawgit.com/MrRio/jsPDF/master/fontconverter/fontconverter.html
+
+```js
+pdf.setFont(opts.customFont);
+```
+
+- **output**
+
+define name of the output PDF file
+
+```js
+pdf.save(opts.output);
+```
+
+- **success**
+
+callback function to do after all code, default will save the file with the output name setting.
---
+++ index.js
@@ -0,0 +1,77 @@
+// reference
+// 1. tutorial: https://www.jianshu.com/p/570c84ee2e8d
+// 2. jsPDF-github: https://github.com/MrRio/jsPDF
+// 3. jsPDF-Documentation: http://raw.githack.com/MrRio/jsPDF/master/docs/
+
+function html2PDF(dom, opts = {}) {
+  // default opts
+  opts = Object.assign({
+    jsPDF: {
+      unit: 'pt',
+      format: 'a4'
+    },
+    imageType: 'image/jpeg',
+    customFont: '',
+    output: 'js.pdf',
+    success: function(pdf) {
+      pdf.save(opts.output);
+    }
+  }, opts);
+
+  // ----- html2canvas -----
+  html2canvas(dom, {
+    onrendered(canvas) {
+      
+      let contentWidth = canvas.width,
+          contentHeight = canvas.height;
+      
+      // 1. height which one page can contain( translate from a4's height to canvas's height )
+      // 2. height which not yet print to PDF.
+      let pageHeight = contentWidth / 595.28 * 841.89,
+          leftHeight = contentHeight;
+      
+      // each page's start position
+      let position = 0;
+
+      // translate content by a4's size
+      let imgWidth = 595.28,
+          imgHeight = 595.28/contentWidth * contentHeight;
+
+      // Canvas to dataUri
+      let pageData = canvas.toDataURL(opts.imageType, 1.0);
+      let images = function(type) {
+        let types = {
+          'image/jpeg': 'JPEG',
+          'image/png': 'PNG',
+          'image/webp': 'WEBP'
+        };
+        return types[type];
+      };
+
+      // ----- jsPDF -----
+      let pdf = new jsPDF(opts.jsPDF);
+      
+      // set custom font, eg: 'chinese', which is made by offical website's tool:
+      // https://rawgit.com/MrRio/jsPDF/master/fontconverter/fontconverter.html
+      pdf.setFont(opts.customFont);
+
+      // check if content needs multi pages
+      if (leftHeight < pageHeight) {
+        pdf.addImage(pageData, images(opts.imageType), 0, 0, imgWidth, imgHeight);
+      } else {
+        while (leftHeight > 0) {
+          pdf.addImage(pageData, images(opts.imageType), 0, position, imgWidth, imgHeight);
+          leftHeight -= pageHeight;
+          position -= 841.89;
+          // check if there's still left content
+          if (leftHeight > 0) {
+            pdf.addPage();
+          }
+        }
+      }
+      
+      // save pdf
+      opts.success(pdf);
+    }
+  })
+}
